5c670785cb58b21532ed53afbe49debb8e05d16c
Jirawong Srisup <jirawong_ss@outlook.co.th>
2023-07-15 14:28:40 +0700

    update

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@ $(document).ready(function () {
     nav: false,
     dots: true,
   });
+  $("#Carousel-1-1").owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    dots: true,
+  });
   $("#Carousel-2").owlCarousel({
     loop: true,
     items: 1,
@@ -27,11 +33,31 @@ $(document).ready(function () {
     nav: false,
     dots: false,
   });
+  $("#Carousel-blog").owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    dots: false,
+  });
   $(".prev-1").click(function () {
-    $("#Carousel-2").trigger("prev.owl.carousel");
+    $("#Carousel-1-1").trigger("prev.owl.carousel");
   });
 
   $(".next-1").click(function () {
+    $("#Carousel-1-1").trigger("next.owl.carousel");
+  });
+  $(".prev-2").click(function () {
+    $("#Carousel-2").trigger("prev.owl.carousel");
+  });
+
+  $(".next-2").click(function () {
     $("#Carousel-2").trigger("next.owl.carousel");
+  });
+  $(".prev-blog").click(function () {
+    $("#Carousel-blog").trigger("prev.owl.carousel");
+  });
+
+  $(".next-blog").click(function () {
+    $("#Carousel-blog").trigger("next.owl.carousel");
   });
 });
